bcee5008f4a5bed8aad635fd4f3722eab44d2482
Jianhua Cheng <w.chengjianhua@gmail.com>
2018-06-27 11:26:22 +0800

    feat: init the project

--- presentation/index.js
+++ presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from "react";
+import React, { Fragment } from "react";
 
 // Import Spectacle Core tags
 import {
@@ -11,7 +11,14 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  Code,
+  Link,
+  Image,
+  Layout,
+  CodePane,
+  Fit,
+  Fill
 } from "spectacle";
 
 // Import theme
@@ -20,53 +27,102 @@ import createTheme from "spectacle/lib/themes/default";
 // Require CSS
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quarternary: "#CECECE"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
-      </Deck>
+      <Fragment>
+        <Deck
+          transition={["zoom", "slide"]}
+          transitionDuration={500}
+          theme={theme}
+        >
+          <Slide transition={["slide"]} bgColor="primary">
+            <Heading size={1} fit caps textColor="secondary">
+              Introducing styled-components
+            </Heading>
+            <Text margin="10px 0 0" textColor="tertiary" bold>
+              <Link
+                textColor="tertiary"
+                href="https://www.styled-components.com/"
+              >
+                👉 Solution by CSS-in-JS 👈
+              </Link>
+            </Text>
+          </Slide>
+
+          <Slide transition={["zoom"]} bgColor="primary">
+            <Heading caps textColor="tertiary" size={5}>
+              in plain css
+            </Heading>
+            <CodePane
+              lang="javascript"
+              source={require("raw-loader!../assets/snippets/css-basic.example")}
+              textSize="24px"
+              margin="1em 0 0"
+            />
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="primary">
+            <Heading caps textColor="tertiary" size={5}>
+              in styled-components
+            </Heading>
+            <CodePane
+              lang="javascript"
+              source={require("raw-loader!../assets/snippets/styled-basic.example")}
+              textSize="24px"
+              margin="1em 0 0"
+            />
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <Heading caps size={2} textColor="primary">
+              <Link
+                textColor="primary"
+                target="_blank"
+                href="https://github.com/cssinjs/istf-spec"
+              >
+                ISTF
+              </Link>
+            </Heading>
+
+            <Text textColor="quarternary" margin="10px 0 0">
+              Interoperable Style Transfer Format
+            </Text>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+              Standard List
+            </Heading>
+            <List>
+              <ListItem>Item 1</ListItem>
+              <ListItem>Item 2</ListItem>
+              <ListItem>Item 3</ListItem>
+              <ListItem>Item 4</ListItem>
+            </List>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote>Example Quote</Quote>
+              <Cite>Author</Cite>
+            </BlockQuote>
+          </Slide>
+        </Deck>
+      </Fragment>
     );
   }
 }
