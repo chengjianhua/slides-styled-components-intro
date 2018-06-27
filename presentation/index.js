37f8c161a1a1d0b285f2c9e0411bc0d8f9088ff3
Jianhua Cheng <w.chengjianhua@gmail.com>
2018-06-27 12:54:42 +0800

    addded almost all slides

--- presentation/index.js
+++ presentation/index.js
@@ -18,6 +18,7 @@ import {
   Layout,
   CodePane,
   Fit,
+  S,
   Fill
 } from "spectacle";
 
@@ -65,7 +66,7 @@ export default class Presentation extends React.Component {
 
           <Slide transition={["zoom"]} bgColor="primary">
             <Heading caps textColor="tertiary" size={5}>
-              in plain css
+              In Plain CSS
             </Heading>
             <CodePane
               lang="javascript"
@@ -77,7 +78,7 @@ export default class Presentation extends React.Component {
 
           <Slide transition={["slide"]} bgColor="primary">
             <Heading caps textColor="tertiary" size={5}>
-              in styled-components
+              In styled-components
             </Heading>
             <CodePane
               lang="javascript"
@@ -85,6 +86,57 @@ export default class Presentation extends React.Component {
               textSize="24px"
               margin="1em 0 0"
             />
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote textSize="32px">
+                Even when using regular CSS or CSS Modules, if you make your
+                styles private to components (as you should), you basically end
+                up with CSS-in-JS anyway.
+              </Quote>
+              <Cite>
+                <Link
+                  target="_blank"
+                  textColor="tertiary"
+                  href="https://twitter.com/markdalgleish"
+                >
+                  @markdalgleish{" "}
+                  <S type="italic" textColor="quarternary">
+                    (🦄 CSS Modules co-creator)
+                  </S>
+                </Link>
+              </Cite>
+            </BlockQuote>
+          </Slide>
+
+          <Slide transition={["fade", "zoom"]}>
+            <Heading size={2} textColor="secondary">
+              Cons
+            </Heading>
+
+            <List textColor="tertiary">
+              <ListItem>
+                <Link
+                  target="_blank"
+                  textColor="tertiary"
+                  href="https://github.com/styled-components/styled-components/pull/1682#issuecomment-399496564"
+                >
+                  Drilled Props
+                </Link>
+              </ListItem>
+
+              <ListItem>
+                <S type="strikethrough">No support for middleware/plugin</S>
+              </ListItem>
+
+              <ListItem>
+                <S type="strikethrough">Performance</S>{" "}
+                <S type="normal" textColor="secondary">
+                  (fast enough)
+                </S>
+              </ListItem>
+            </List>
           </Slide>
 
           <Slide transition={["fade"]} bgColor="tertiary">
@@ -103,23 +155,94 @@ export default class Presentation extends React.Component {
             </Text>
           </Slide>
 
-          <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-            <Heading size={6} textColor="secondary" caps>
-              Standard List
+          <Slide transition={["slide"]} bgColor="primary">
+            <Heading size={6} textColor="tertiary" caps>
+              Features
             </Heading>
-            <List>
-              <ListItem>Item 1</ListItem>
-              <ListItem>Item 2</ListItem>
-              <ListItem>Item 3</ListItem>
-              <ListItem>Item 4</ListItem>
+
+            <List textColor="secondary">
+              <ListItem>定义了一种 CSS-in-JS 的输出格式</ListItem>
+              <ListItem>使得 in-JS 的 CSS 能够脱离 runtime 环境被共享</ListItem>
+              <ListItem>
+                发布 npm 包时可以输出为该格式, 将大幅提升 runtime
+                计算出样式的速度
+              </ListItem>
+              <ListItem>运行时的解析速度够快, 运行时的插件也成为可能</ListItem>
             </List>
           </Slide>
 
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <BlockQuote>
-              <Quote>Example Quote</Quote>
-              <Cite>Author</Cite>
-            </BlockQuote>
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <Heading caps size={2} textColor="primary">
+              <Link
+                textColor="primary"
+                target="_blank"
+                href="https://github.com/cssinjs/istf-spec"
+              >
+                ISTF
+              </Link>
+            </Heading>
+
+            <Text textColor="quarternary" margin="10px 0 0">
+              Interoperable Style Transfer Format
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide", "zoom"]} bgColor="quarternary">
+            <Heading textColor="secondary" caps size={5}>
+              ⚡️ Blazing Fast ⚡️
+            </Heading>
+
+            <Link
+              target="_blank"
+              href="https://esbench.com/bench/592d599e99634800a03483d8"
+            >
+              <Image
+                margin="1em 0 0"
+                src={require("../assets/istf-benchmark.png")}
+              />
+            </Link>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <Heading size={4} textColor="primary">
+              <Link
+                textColor="primary"
+                target="_blank"
+                href="https://github.com/kitten/sweetsour"
+              >
+                kitten/sweetsour
+              </Link>
+            </Heading>
+
+            <Text margin="1em 0 0" textColor="quarternary">
+              A CSS-in-JS parser and pipeline adhering to the ISTF spec 🍭
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide", "fade"]}>
+            <Heading textColor="tertiary" size={5}>
+              Input
+            </Heading>
+
+            <CodePane
+              lang="javascript"
+              source={require("raw-loader!../assets/snippets/sweetsour-input.snippet")}
+              textSize="24px"
+              margin="1em 0 0"
+            />
+          </Slide>
+
+          <Slide transition={["slide", "fade"]}>
+            <Heading size={5} textColor="tertiary">
+              Output
+            </Heading>
+
+            <CodePane
+              lang="javascript"
+              source={require("raw-loader!../assets/snippets/sweetsour-output.snippet")}
+              textSize="20px"
+              margin="1em 0 0"
+            />
           </Slide>
         </Deck>
       </Fragment>
